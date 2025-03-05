289d38a462fd16b972936dbe7591ec50aa35162a
zhaoxinfang <zhaoxinfang>
2025-03-05 18:09:07 +0800

    add alien_invasion.py file

---
+++ 1_python/1_python编程_从入门到实践_第3版/alien_invasion/chapter12/alien_invasion.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((800, 640))
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """开始游戏主循环"""
+        while True:
+            # 侦听键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # 每次循环时都会重绘屏幕
+            self.screen.fill(self.bg_color)
+
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    # 创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
+
+
+
